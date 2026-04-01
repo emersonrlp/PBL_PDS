3c0f131880157ae435d8495a5e138c597518810c
Emanuel Antonio Lima Pereira <emanuelantoniolimapereira@gmail.com>
2026-04-01 12:37:35 -0300

    feat: update dtft and update plots

--- pbl-pds/main2.m
+++ pbl-pds/main2.m
@@ -72,15 +72,38 @@ figure('Name', 'x1: Superamostragem e Interpolação (15kHz -> 30kHz)', 'NumberT
 janela1 = 1000:1100;
 janela1_up = (1000*L1):(1100*L1);
 
-subplot(3,2,1); stem(t1(janela1), x1_15k(janela1), 'b', 'filled', 'MarkerSize', 4); title('1. Original x1 (15 kHz)'); grid on;
-subplot(3,2,2); plot(w_vec, abs(dtft_pure(x1_15k(:), w_vec)), 'b', 'LineWidth', 1.5); title('1. Espectro Original x1'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- 1. Criando os vetores de frequência em Hz antes de plotar ---
+Fs1 = 15000; % Frequência original
+Fs2 = 30000; % Frequência após upsampling (L=2)
+f_vec_15k = (w_vec * Fs1) / (2*pi);
+f_vec_30k = (w_vec * Fs2) / (2*pi);
 
-subplot(3,2,3); stem(t1_up(janela1_up), x1_up(janela1_up), 'r', 'filled', 'MarkerSize', 4); title('2. x1 com Zeros (L=2)'); grid on;
-subplot(3,2,4); plot(w_vec, abs(dtft_pure(x1_up(:), w_vec)), 'r', 'LineWidth', 1.5); title('2. Espectro com Imagens'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% =========================================================================
+% ETAPA 1: SINAL ORIGINAL
+% =========================================================================
+% Seu código original (mantido):
+subplot(3,3,1); stem(t1(janela1), x1_15k(janela1), 'b', 'filled', 'MarkerSize', 4); title('1. Original x1 (15 kHz)'); grid on;
+subplot(3,3,2); plot(w_vec, abs(dtft_pure(x1_15k(:), w_vec)), 'b', 'LineWidth', 1.5); title('1. Espectro Original x1'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- NOVO PLOT ADICIONADO (EM HZ) ---
+subplot(3,3,3); plot(f_vec_15k, abs(dtft_pure(x1_15k(:), w_vec)), 'b', 'LineWidth', 1.5); title('1. Espectro Original (Hz)'); xlabel('Frequência (Hz)'); xlim([-Fs1 Fs1]); grid on;
 
-subplot(3,2,5); stem(t1_up(janela1_up), x1_interpolado(janela1_up), 'r', 'filled', 'MarkerSize', 4); title('3. x1 Interpolado (30 kHz)'); grid on;
-subplot(3,2,6); plot(w_vec, abs(dtft_pure(x1_interpolado(:), w_vec)), 'g', 'LineWidth', 1.5); title('3. Espectro Interpolado (Limpo)'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% =========================================================================
+% ETAPA 2: SINAL COM ZEROS (UPSAMPLING)
+% =========================================================================
+% Seu código original (mantido):
+subplot(3,3,4); stem(t1_up(janela1_up), x1_up(janela1_up), 'r', 'filled', 'MarkerSize', 4); title('2. x1 com Zeros (L=2)'); grid on;
+subplot(3,3,5); plot(w_vec, abs(dtft_pure(x1_up(:), w_vec)), 'r', 'LineWidth', 1.5); title('2. Espectro com Imagens'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- NOVO PLOT ADICIONADO (EM HZ) ---
+subplot(3,3,6); plot(f_vec_30k, abs(dtft_pure(x1_up(:), w_vec)), 'r', 'LineWidth', 1.5); title('2. Espectro com Imagens (Hz)'); xlabel('Frequência (Hz)'); xlim([-Fs2 Fs2]); grid on;
 
+% =========================================================================
+% ETAPA 3: SINAL INTERPOLADO (FILTRO PASSA-BAIXA)
+% =========================================================================
+% Seu código original (mantido):
+subplot(3,3,7); stem(t1_up(janela1_up), x1_interpolado(janela1_up), 'r', 'filled', 'MarkerSize', 4); title('3. x1 Interpolado (30 kHz)'); grid on;
+subplot(3,3,8); plot(w_vec, abs(dtft_pure(x1_interpolado(:), w_vec)), 'g', 'LineWidth', 1.5); title('3. Espectro Interpolado (Limpo)'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- NOVO PLOT ADICIONADO (EM HZ) ---
+subplot(3,3,9); plot(f_vec_30k, abs(dtft_pure(x1_interpolado(:), w_vec)), 'g', 'LineWidth', 1.5); title('3. Espectro Interpolado (Hz)'); xlabel('Frequência (Hz)'); xlim([-Fs2 Fs2]); grid on;
 % =========================================================================
 % VISUALIZAÇÃO 2: SINAL x2 (10k -> 30k)
 % =========================================================================
@@ -89,11 +112,35 @@ figure('Name', 'x2: Superamostragem e Interpolação (10kHz -> 30kHz)', 'NumberT
 janela2 = 1000:1100;
 janela2_up = (1000*L2):(1100*L2);
 
-subplot(3,2,1); stem(t2(janela2), x2_10k(janela2), 'b', 'filled', 'MarkerSize', 4); title('1. Original x2 (10 kHz)'); grid on;
-subplot(3,2,2); plot(w_vec, abs(dtft_pure(x2_10k(:), w_vec)), 'b', 'LineWidth', 1.5); title('1. Espectro Original x2'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- 1. Criando os vetores de frequência em Hz antes de plotar ---
+Fs1_x2 = 10000; % Frequência original de x2 (10 kHz)
+Fs2_x2 = 30000; % Frequência após upsampling (L=3 -> 10k * 3 = 30 kHz)
+f_vec_10k = (w_vec * Fs1_x2) / (2*pi);
+f_vec_30k = (w_vec * Fs2_x2) / (2*pi);
 
-subplot(3,2,3); stem(t2_up(janela2_up), x2_up(janela2_up), 'r', 'filled', 'MarkerSize', 4); title('2. x2 com Zeros (L=3)'); grid on;
-subplot(3,2,4); plot(w_vec, abs(dtft_pure(x2_up(:), w_vec)), 'r', 'LineWidth', 1.5); title('2. Espectro com Imagens (2 Cópias!)'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% =========================================================================
+% ETAPA 1: SINAL ORIGINAL x2
+% =========================================================================
+% Seu código original (mantido):
+subplot(3,3,1); stem(t2(janela2), x2_10k(janela2), 'b', 'filled', 'MarkerSize', 4); title('1. Original x2 (10 kHz)'); grid on;
+subplot(3,3,2); plot(w_vec, abs(dtft_pure(x2_10k(:), w_vec)), 'b', 'LineWidth', 1.5); title('1. Espectro Original x2'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- NOVO PLOT ADICIONADO (EM HZ) ---
+subplot(3,3,3); plot(f_vec_10k, abs(dtft_pure(x2_10k(:), w_vec)), 'b', 'LineWidth', 1.5); title('1. Espectro Original (Hz)'); xlabel('Frequência (Hz)'); xlim([-Fs1_x2 Fs1_x2]); grid on;
 
-subplot(3,2,5); stem(t2_up(janela2_up), x2_interpolado(janela2_up), 'r', 'filled', 'MarkerSize', 4); title('3. x2 Interpolado (30 kHz)'); grid on;
-subplot(3,2,6); plot(w_vec, abs(dtft_pure(x2_interpolado(:), w_vec)), 'g', 'LineWidth', 1.5); title('3. Espectro Interpolado (Limpo)'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% =========================================================================
+% ETAPA 2: SINAL COM ZEROS (UPSAMPLING L=3)
+% =========================================================================
+% Seu código original (mantido):
+subplot(3,3,4); stem(t2_up(janela2_up), x2_up(janela2_up), 'r', 'filled', 'MarkerSize', 4); title('2. x2 com Zeros (L=3)'); grid on;
+subplot(3,3,5); plot(w_vec, abs(dtft_pure(x2_up(:), w_vec)), 'r', 'LineWidth', 1.5); title('2. Espectro com Imagens (2 Cópias!)'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- NOVO PLOT ADICIONADO (EM HZ) ---
+subplot(3,3,6); plot(f_vec_30k, abs(dtft_pure(x2_up(:), w_vec)), 'r', 'LineWidth', 1.5); title('2. Espectro com Imagens (Hz)'); xlabel('Frequência (Hz)'); xlim([-Fs2_x2 Fs2_x2]); grid on;
+
+% =========================================================================
+% ETAPA 3: SINAL INTERPOLADO (FILTRO PASSA-BAIXA)
+% =========================================================================
+% Seu código original (mantido):
+subplot(3,3,7); stem(t2_up(janela2_up), x2_interpolado(janela2_up), 'r', 'filled', 'MarkerSize', 4); title('3. x2 Interpolado (30 kHz)'); grid on;
+subplot(3,3,8); plot(w_vec, abs(dtft_pure(x2_interpolado(:), w_vec)), 'g', 'LineWidth', 1.5); title('3. Espectro Interpolado (Limpo)'); xlim([-3*pi 3*pi]); xticks([-3*pi, -2*pi, -pi, 0, pi, 2*pi, 3*pi]); xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'}); grid on;
+% --- NOVO PLOT ADICIONADO (EM HZ) ---
+subplot(3,3,9); plot(f_vec_30k, abs(dtft_pure(x2_interpolado(:), w_vec)), 'g', 'LineWidth', 1.5); title('3. Espectro Interpolado (Hz)'); xlabel('Frequência (Hz)'); xlim([-Fs2_x2 Fs2_x2]); grid on;
